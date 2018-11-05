5c474817364c942eaeb5ce749ef302a84d731ef2
Cameron Watters <watters@gmail.com>
2018-11-05 09:53:50 -0800

    add agent signing (tested for RSA keys only)

--- src/Watters.SSHAgent.Client/SSHAgentClient.cs
+++ src/Watters.SSHAgent.Client/SSHAgentClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -56,7 +57,7 @@ namespace Watters.SSHAgent.Client
         public ReadOnlyCollection<Identity> List()
         {
             SendRequestAndValidateResponse(
-                messageNumber: SSH_AGENTC_REQUEST_IDENTITIES,
+                request: new byte[] {SSH_AGENTC_REQUEST_IDENTITIES},
                 expectedResponse: SSH_AGENT_IDENTITIES_ANSWER
             );
 
@@ -91,14 +92,127 @@ namespace Watters.SSHAgent.Client
             return identities.AsReadOnly();
         }
 
+         /*
+         * https://tools.ietf.org/html/draft-miller-ssh-agent-00#section-4.5
+         * 
+         * 4.5.  Private key operations
+         *
+         *    A client may request the agent perform a private key signature
+         *    operation using the following message:
+         *
+         *        byte                    SSH_AGENTC_SIGN_REQUEST
+         *        string                  key blob
+         *        string                  data
+         *        uint32                  flags
+         *
+         *    Where "key blob" is the key requested to perform the signature,
+         *    "data" is the data to be signed and "flags" is a bitfield containing
+         *    the bitwise OR of zero or more signature flags (see below).
+         *
+         *    If the agent is unable or unwilling to generate the signature (e.g.
+         *    because it doesn't have the specified key, or the user refused
+         *    confirmation of a constrained key), it must reply with a
+         *    SSH_AGENT_FAILURE message.
+         *
+         *    On success, the agent shall reply with:
+         * 
+         *        byte                    SSH_AGENT_SIGN_RESPONSE
+         *        string                  signature
+         *
+         *    The signature format is specific to the algorithm of the key type in
+         *    use.  SSH protocol signature formats are defined in [RFC4253] for
+         *    "ssh-rsa" and "ssh-dss" keys, in [RFC5656] for "ecdsa-sha2-*" keys
+         *    and in [I-D.ietf-curdle-ssh-ed25519] for "ssh-ed25519" keys.
+         *
+         * 4.5.1.  Signature flags
+         *
+         *    Two flags are currently defined for signature request messages:
+         *    SSH_AGENT_RSA_SHA2_256 and SSH_AGENT_RSA_SHA2_512.  These two flags
+         *    are only valid for "ssh-rsa" keys and request that the agent return a
+         *    signature using the "rsa-sha2-256" or "rsa-sha2-515" signature
+         *    methods respectively.  These signature schemes are defined in
+         *    [I-D.ietf-curdle-rsa-sha2].
+         */
+        
+        public SignResponse Sign(Identity identity, byte[] data)
+        {
+            using (var request = new MemoryStream())
+            {
+                request.WriteByte(SSH_AGENTC_SIGN_REQUEST);
+                
+                // key blob = uint32 length of key bytes + key bytes
+                request.Write(
+                    buffer: ToNetworkByteOrderBytes((uint) identity.KeyBlob.Length), 
+                    offset: 0, 
+                    count: 4
+                );
+                request.Write(
+                    buffer: identity.KeyBlob,
+                    offset: 0,
+                    count: identity.KeyBlob.Length
+                );
+                
+                // data = uint32 length of data + data
+                request.Write(
+                    buffer: ToNetworkByteOrderBytes((uint) data.Length),
+                    offset: 0,
+                    count: 4);
+                request.Write(
+                    buffer: data,
+                    offset: 0,
+                    count:data.Length
+                );
+                
+                // write flags -- uint32 of flags
+                request.Write(new byte[] {0, 0, 0, 0}, 0, 4);
+                
+                var remainingResponseSize = SendRequestAndValidateResponse(request.ToArray(),SSH_AGENT_SIGN_RESPONSE);
+
+                byte[] response = new byte[remainingResponseSize];
+                _socket.Receive(response);
+
+                using (var responseStream = new MemoryStream(response))
+                {
+                    var signatureStructLengthBytes = new byte[4];
+                    responseStream.Read(signatureStructLengthBytes, 0, signatureStructLengthBytes.Length);
+                    uint signatureStructLength = ToNetworkByteOrderUInt32(signatureStructLengthBytes,0);
+
+                    var formatLengthBytes = new byte[4];
+                    responseStream.Read(formatLengthBytes, 0, formatLengthBytes.Length);
+                    uint formatLength = ToNetworkByteOrderUInt32(formatLengthBytes,0);
+
+                    var formatBytes = new byte[formatLength];
+                    responseStream.Read(formatBytes, 0, formatBytes.Length);
+                    string format = Encoding.UTF8.GetString(formatBytes);
+
+                    var signatureLengthBytes = new byte[4];
+                    responseStream.Read(signatureLengthBytes, 0, signatureLengthBytes.Length);
+                    uint signatureLength = ToNetworkByteOrderUInt32(signatureLengthBytes, 0);
+
+                    var signatureBytes = new byte[signatureLength];
+                    responseStream.Read(signatureBytes, 0, signatureBytes.Length);
+                    
+                    return new SignResponse
+                    {
+                        Kind = format,
+                        Signature = signatureBytes
+                    };
+                }
+            }
+        }
+        
         public void Dispose()
         {
             _socket.Dispose();
         }
 
-        private void SendRequestAndValidateResponse(byte messageNumber, byte expectedResponse)
+        private uint SendRequestAndValidateResponse(byte[] request, byte expectedResponse)
         {
-            byte[] message = {0, 0, 0, 1, messageNumber};
+            byte[] message = new byte[4 + request.Length];
+            byte[] messageNumberLength = ToNetworkByteOrderBytes((uint) request.Length);
+            
+            Buffer.BlockCopy(messageNumberLength,0,message,0,messageNumberLength.Length);
+            Buffer.BlockCopy(request,0,message,messageNumberLength.Length,request.Length);
 
             _socket.Send(message);
 
@@ -122,6 +236,17 @@ namespace Watters.SSHAgent.Client
                 throw new InvalidOperationException($"Agent responded with unexpected" +
                                                     $" response code ({responseCode}) rather than the expected" +
                                                     $" ({expectedResponse})");
+
+            return responseSize - (uint) responseCode.Length;
+        }
+
+        private byte[] ToNetworkByteOrderBytes(uint length)
+        {
+            byte[] bytes = BitConverter.GetBytes(length);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+
+            return bytes;
         }
 
         private uint ToNetworkByteOrderUInt32(byte[] bytes, int startIndex)
@@ -143,6 +268,12 @@ namespace Watters.SSHAgent.Client
         private const byte SSH_AGENTC_SIGN_REQUEST = 13;
         private const byte SSH_AGENT_SIGN_RESPONSE = 14;
 
+        public class SignResponse
+        {
+            public string Kind { get; set; }
+            public byte[] Signature { get; set; }
+        }
+        
         public class Identity
         {
             public byte[] KeyBlob { get; set; }
--- tests/Watters.SSHAgent.Client.Tests/ClientTests.cs
+++ tests/Watters.SSHAgent.Client.Tests/ClientTests.cs
@@ -1,6 +1,11 @@
 using System;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
+using System.Text;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
 using Xunit;
 
 namespace Watters.SSHAgent.Client.Tests
@@ -16,8 +21,42 @@ namespace Watters.SSHAgent.Client.Tests
                 var identities = client.List();
 
                 string mantaKeyId = Environment.GetEnvironmentVariable("MANTA_KEY_ID");
-
                 var mantaKey = identities.Single(i => i.BlobMD5 == mantaKeyId);
+            }
+        }
+
+        [Fact]
+        public void CheckSignForRSA()
+        {
+            using (var client = new SSHAgentClient())
+            {
+                client.Connect(new UnixDomainSocketEndPoint(Environment.GetEnvironmentVariable("SSH_AUTH_SOCK")));
+
+                var identities = client.List();
+                
+                string homeDir = Environment.GetEnvironmentVariable("HOME");
+                string rsaKeyFilePath = $"{homeDir}/.ssh/id_rsa";
+
+                var rsaAgentKey = client.List().SingleOrDefault(i => i.CommentUTF8 == rsaKeyFilePath);
+
+                AsymmetricCipherKeyPair key;
+                using (StreamReader keyFileReader = File.OpenText(rsaKeyFilePath))
+                {
+                    key = new PemReader(keyFileReader).ReadObject() as AsymmetricCipherKeyPair;
+
+                    if (key == null)
+                        throw new ArgumentException("Key file is not a valid key.", nameof(rsaKeyFilePath));
+                }
+                
+                ISigner signer = SignerUtilities.GetSigner("SHA1WITHRSA");
+                signer.Init(true, key.Private);
+
+                var input = Encoding.UTF8.GetBytes("Hello, World!");
+                signer.BlockUpdate(input, 0, input.Length);
+                byte[] privateKeySignature = signer.GenerateSignature();
+                byte[] agentSignature = client.Sign(rsaAgentKey, input).Signature;
+
+                Assert.Equal(agentSignature,privateKeySignature);
             }
         }
     }
