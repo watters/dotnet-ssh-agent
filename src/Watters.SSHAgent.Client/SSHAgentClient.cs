b0919676012ecbb9ce2ab881c08497cc57d61342
Cameron Watters <watters@gmail.com>
2018-11-05 16:06:44 -0800

    cleanup whitespace and some formatting

--- src/Watters.SSHAgent.Client/SSHAgentClient.cs
+++ src/Watters.SSHAgent.Client/SSHAgentClient.cs
@@ -17,16 +17,12 @@ namespace Watters.SSHAgent.Client
 
     public partial class SSHAgentClient : IDisposable
     {
-        public SSHAgentClient()
+        public SSHAgentClient(EndPoint endpoint)
         {
             _socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
-        }
-
-        public void Connect(EndPoint endpoint)
-        {
             _socket.Connect(endpoint);
         }
-        
+
         /*
          * https://tools.ietf.org/html/draft-miller-ssh-agent-00#section-4.4
          *
@@ -55,14 +51,11 @@ namespace Watters.SSHAgent.Client
 
         public ReadOnlyCollection<Identity> List()
         {
-            SendRequestAndValidateResponse(
-                request: new byte[] {SSH_AGENTC_REQUEST_IDENTITIES},
-                expectedResponse: SSH_AGENT_IDENTITIES_ANSWER
-            );
+            SendRequestAndValidateResponse(new byte[] {SSH_AGENTC_REQUEST_IDENTITIES}, SSH_AGENT_IDENTITIES_ANSWER);
 
             var countOfIdentitiesBytes = new byte[4];
             _socket.Receive(countOfIdentitiesBytes);
-            var countOfIdentities = ToNetworkByteOrderUInt32(countOfIdentitiesBytes,0);
+            var countOfIdentities = ToNetworkByteOrderUInt32(countOfIdentitiesBytes, 0);
 
             var identities = new List<Identity>();
             for (var i = 0; i < countOfIdentities; i++)
@@ -81,124 +74,103 @@ namespace Watters.SSHAgent.Client
                 var commentBytes = new byte[commentLength];
                 _socket.Receive(commentBytes);
 
-                identities.Add(new Identity
-                (
-                    keyBlob: blobBytes,
-                    comment: commentBytes
-                ));
+                identities.Add(new Identity(commentBytes, blobBytes));
             }
 
             return identities.AsReadOnly();
         }
 
-         /*
-         * https://tools.ietf.org/html/draft-miller-ssh-agent-00#section-4.5
-         * 
-         * 4.5.  Private key operations
-         *
-         *    A client may request the agent perform a private key signature
-         *    operation using the following message:
-         *
-         *        byte                    SSH_AGENTC_SIGN_REQUEST
-         *        string                  key blob
-         *        string                  data
-         *        uint32                  flags
-         *
-         *    Where "key blob" is the key requested to perform the signature,
-         *    "data" is the data to be signed and "flags" is a bitfield containing
-         *    the bitwise OR of zero or more signature flags (see below).
-         *
-         *    If the agent is unable or unwilling to generate the signature (e.g.
-         *    because it doesn't have the specified key, or the user refused
-         *    confirmation of a constrained key), it must reply with a
-         *    SSH_AGENT_FAILURE message.
-         *
-         *    On success, the agent shall reply with:
-         * 
-         *        byte                    SSH_AGENT_SIGN_RESPONSE
-         *        string                  signature
-         *
-         *    The signature format is specific to the algorithm of the key type in
-         *    use.  SSH protocol signature formats are defined in [RFC4253] for
-         *    "ssh-rsa" and "ssh-dss" keys, in [RFC5656] for "ecdsa-sha2-*" keys
-         *    and in [I-D.ietf-curdle-ssh-ed25519] for "ssh-ed25519" keys.
-         *
-         * 4.5.1.  Signature flags
-         *
-         *    Two flags are currently defined for signature request messages:
-         *    SSH_AGENT_RSA_SHA2_256 and SSH_AGENT_RSA_SHA2_512.  These two flags
-         *    are only valid for "ssh-rsa" keys and request that the agent return a
-         *    signature using the "rsa-sha2-256" or "rsa-sha2-515" signature
-         *    methods respectively.  These signature schemes are defined in
-         *    [I-D.ietf-curdle-rsa-sha2].
-         */
-        
+        /*
+        * https://tools.ietf.org/html/draft-miller-ssh-agent-00#section-4.5
+        *
+        * 4.5.  Private key operations
+        *
+        *    A client may request the agent perform a private key signature
+        *    operation using the following message:
+        *
+        *        byte                    SSH_AGENTC_SIGN_REQUEST
+        *        string                  key blob
+        *        string                  data
+        *        uint32                  flags
+        *
+        *    Where "key blob" is the key requested to perform the signature,
+        *    "data" is the data to be signed and "flags" is a bitfield containing
+        *    the bitwise OR of zero or more signature flags (see below).
+        *
+        *    If the agent is unable or unwilling to generate the signature (e.g.
+        *    because it doesn't have the specified key, or the user refused
+        *    confirmation of a constrained key), it must reply with a
+        *    SSH_AGENT_FAILURE message.
+        *
+        *    On success, the agent shall reply with:
+        *
+        *        byte                    SSH_AGENT_SIGN_RESPONSE
+        *        string                  signature
+        *
+        *    The signature format is specific to the algorithm of the key type in
+        *    use.  SSH protocol signature formats are defined in [RFC4253] for
+        *    "ssh-rsa" and "ssh-dss" keys, in [RFC5656] for "ecdsa-sha2-*" keys
+        *    and in [I-D.ietf-curdle-ssh-ed25519] for "ssh-ed25519" keys.
+        *
+        * 4.5.1.  Signature flags
+        *
+        *    Two flags are currently defined for signature request messages:
+        *    SSH_AGENT_RSA_SHA2_256 and SSH_AGENT_RSA_SHA2_512.  These two flags
+        *    are only valid for "ssh-rsa" keys and request that the agent return a
+        *    signature using the "rsa-sha2-256" or "rsa-sha2-515" signature
+        *    methods respectively.  These signature schemes are defined in
+        *    [I-D.ietf-curdle-rsa-sha2].
+        */
+
         public SignResponse Sign(Identity identity, byte[] data)
         {
+            byte[] requestBytes;
             using (var request = new MemoryStream())
             {
                 request.WriteByte(SSH_AGENTC_SIGN_REQUEST);
-                
+
                 // key blob = uint32 length of key bytes + key bytes
-                request.Write(
-                    buffer: ToNetworkByteOrderBytes((uint) identity.KeyBlob.Length), 
-                    offset: 0, 
-                    count: 4
-                );
-                request.Write(
-                    buffer: identity.KeyBlob,
-                    offset: 0,
-                    count: identity.KeyBlob.Length
-                );
-                
+                request.Write(ToNetworkByteOrderBytes((uint) identity.KeyBlob.Length), 0, 4);
+                request.Write(identity.KeyBlob, 0, identity.KeyBlob.Length);
+
                 // data = uint32 length of data + data
-                request.Write(
-                    buffer: ToNetworkByteOrderBytes((uint) data.Length),
-                    offset: 0,
-                    count: 4);
-                request.Write(
-                    buffer: data,
-                    offset: 0,
-                    count:data.Length
-                );
-                
+                request.Write(ToNetworkByteOrderBytes((uint) data.Length), 0, 4);
+                request.Write(data, 0, data.Length);
+
                 // write flags -- uint32 of flags
                 request.Write(new byte[] {0, 0, 0, 0}, 0, 4);
-                
-                var remainingResponseSize = SendRequestAndValidateResponse(request.ToArray(),SSH_AGENT_SIGN_RESPONSE);
 
-                byte[] response = new byte[remainingResponseSize];
-                _socket.Receive(response);
+                requestBytes = request.ToArray();
+            }
 
-                using (var responseStream = new MemoryStream(response))
-                {
-                    var signatureStructLengthBytes = new byte[4];
-                    responseStream.Read(signatureStructLengthBytes, 0, signatureStructLengthBytes.Length);
+            var remainingResponseSize = SendRequestAndValidateResponse(requestBytes, SSH_AGENT_SIGN_RESPONSE);
+            byte[] response = new byte[remainingResponseSize];
+            _socket.Receive(response);
 
-                    var formatLengthBytes = new byte[4];
-                    responseStream.Read(formatLengthBytes, 0, formatLengthBytes.Length);
-                    uint formatLength = ToNetworkByteOrderUInt32(formatLengthBytes,0);
+            using (var responseStream = new MemoryStream(response))
+            {
+                var signatureStructLengthBytes = new byte[4];
+                responseStream.Read(signatureStructLengthBytes, 0, signatureStructLengthBytes.Length);
 
-                    var formatBytes = new byte[formatLength];
-                    responseStream.Read(formatBytes, 0, formatBytes.Length);
-                    string format = Encoding.UTF8.GetString(formatBytes);
+                var formatLengthBytes = new byte[4];
+                responseStream.Read(formatLengthBytes, 0, formatLengthBytes.Length);
+                uint formatLength = ToNetworkByteOrderUInt32(formatLengthBytes, 0);
 
-                    var signatureLengthBytes = new byte[4];
-                    responseStream.Read(signatureLengthBytes, 0, signatureLengthBytes.Length);
-                    uint signatureLength = ToNetworkByteOrderUInt32(signatureLengthBytes, 0);
+                var formatBytes = new byte[formatLength];
+                responseStream.Read(formatBytes, 0, formatBytes.Length);
+                string format = Encoding.UTF8.GetString(formatBytes);
 
-                    var signatureBytes = new byte[signatureLength];
-                    responseStream.Read(signatureBytes, 0, signatureBytes.Length);
-                    
-                    return new SignResponse
-                    (
-                        format: format,
-                        signature: signatureBytes
-                    );
-                }
+                var signatureLengthBytes = new byte[4];
+                responseStream.Read(signatureLengthBytes, 0, signatureLengthBytes.Length);
+                uint signatureLength = ToNetworkByteOrderUInt32(signatureLengthBytes, 0);
+
+                var signatureBytes = new byte[signatureLength];
+                responseStream.Read(signatureBytes, 0, signatureBytes.Length);
+
+                return new SignResponse(format, signatureBytes);
             }
         }
-        
+
         public void Dispose()
         {
             _socket.Dispose();
@@ -208,9 +180,9 @@ namespace Watters.SSHAgent.Client
         {
             byte[] message = new byte[4 + request.Length];
             byte[] messageNumberLength = ToNetworkByteOrderBytes((uint) request.Length);
-            
-            Buffer.BlockCopy(messageNumberLength,0,message,0,messageNumberLength.Length);
-            Buffer.BlockCopy(request,0,message,messageNumberLength.Length,request.Length);
+
+            Buffer.BlockCopy(messageNumberLength, 0, message, 0, messageNumberLength.Length);
+            Buffer.BlockCopy(request, 0, message, messageNumberLength.Length, request.Length);
 
             _socket.Send(message);
 
@@ -250,19 +222,19 @@ namespace Watters.SSHAgent.Client
         private uint ToNetworkByteOrderUInt32(byte[] bytes, int startIndex)
         {
             byte[] myBytes = new byte[bytes.Length];
-            Buffer.BlockCopy(bytes,startIndex,myBytes,0,bytes.Length - startIndex);
+            Buffer.BlockCopy(bytes, startIndex, myBytes, 0, bytes.Length - startIndex);
             if (BitConverter.IsLittleEndian)
                 Array.Reverse(myBytes);
 
             return BitConverter.ToUInt32(myBytes, startIndex);
         }
 
-        private Socket _socket;
+        private readonly Socket _socket;
 
         private const byte SSH_AGENT_FAILURE = 5;
         private const byte SSH_AGENTC_REQUEST_IDENTITIES = 11;
         private const byte SSH_AGENT_IDENTITIES_ANSWER = 12;
         private const byte SSH_AGENTC_SIGN_REQUEST = 13;
         private const byte SSH_AGENT_SIGN_RESPONSE = 14;
-    }    
+    }
 }
--- tests/Watters.SSHAgent.Client.Tests/ClientTests.cs
+++ tests/Watters.SSHAgent.Client.Tests/ClientTests.cs
@@ -10,60 +10,80 @@ using Xunit;
 
 namespace Watters.SSHAgent.Client.Tests
 {
-    public class ClientTests
+    public class ClientTests : IDisposable
     {
+        public ClientTests()
+        {
+            _client = new SSHAgentClient(
+                new UnixDomainSocketEndPoint(Environment.GetEnvironmentVariable("SSH_AUTH_SOCK")));
+        }
+
         [Fact]
         public void CheckListIdentities()
         {
-            using (var client = new SSHAgentClient())
-            {
-                client.Connect(new UnixDomainSocketEndPoint(Environment.GetEnvironmentVariable("SSH_AUTH_SOCK")));
-                var identities = client.List();
-
-                string md5Fingerprint = Environment.GetEnvironmentVariable("MD5_FINGERPRINT");
-                var md5Result = identities.SingleOrDefault(i => $"MD5:{i.BlobMD5}" == md5Fingerprint);
-
-                string sha256Fingerprint = Environment.GetEnvironmentVariable("SHA256_FINGERPRINT");
-                var sha256Result = identities.SingleOrDefault(i => $"SHA256:{i.BlobSHA256}" == sha256Fingerprint);
-
-                Assert.NotNull(md5Result);
-                Assert.NotNull(sha256Result);
-            }
+            Assert.True(_client.List().Any());
         }
 
         [Fact]
-        public void CheckSignForRSA()
+        public void CheckSHA256Fingerprint()
         {
-            using (var client = new SSHAgentClient())
-            {
-                client.Connect(new UnixDomainSocketEndPoint(Environment.GetEnvironmentVariable("SSH_AUTH_SOCK")));
-
-                var identities = client.List();
-                
-                string homeDir = Environment.GetEnvironmentVariable("HOME");
-                string rsaKeyFilePath = $"{homeDir}/.ssh/id_rsa";
-
-                var rsaAgentKey = identities.SingleOrDefault(i => i.CommentUTF8 == rsaKeyFilePath);
-
-                AsymmetricCipherKeyPair key;
-                using (StreamReader keyFileReader = File.OpenText(rsaKeyFilePath))
-                {
-                    key = new PemReader(keyFileReader).ReadObject() as AsymmetricCipherKeyPair;
-
-                    if (key == null)
-                        throw new ArgumentException("Key file is not a valid key.", nameof(rsaKeyFilePath));
-                }
-                
-                ISigner signer = SignerUtilities.GetSigner("SHA1WITHRSA");
-                signer.Init(true, key.Private);
-
-                var input = Encoding.UTF8.GetBytes("Hello, World!");
-                signer.BlockUpdate(input, 0, input.Length);
-                byte[] privateKeySignature = signer.GenerateSignature();
-                byte[] agentSignature = client.Sign(rsaAgentKey, input).Signature;
-
-                Assert.Equal(agentSignature,privateKeySignature);
-            }
+            Assert.NotNull(
+                _client.List().SingleOrDefault(
+                    i => $"SHA256:{i.BlobSHA256}" == Environment.GetEnvironmentVariable("SHA256_FINGERPRINT")
+                ));
         }
+
+        [Fact]
+        public void CheckMD5Fingerprint()
+        {
+            Assert.NotNull(
+                _client.List().SingleOrDefault(
+                    i => $"MD5:{i.BlobMD5}" == Environment.GetEnvironmentVariable("MD5_FINGERPRINT")
+                ));
+        }
+
+        [Fact]
+        public void CheckSignWithRSAKey()
+        {
+            var identities = _client.List();
+
+            string homeDir = Environment.GetEnvironmentVariable("HOME");
+            string rsaKeyFilePath = $"{homeDir}/.ssh/id_rsa";
+
+            var rsaAgentKey = identities.SingleOrDefault(i => i.CommentUTF8 == rsaKeyFilePath);
+
+            AsymmetricCipherKeyPair key;
+            using (StreamReader keyFileReader = File.OpenText(rsaKeyFilePath))
+            {
+                key = new PemReader(keyFileReader).ReadObject() as AsymmetricCipherKeyPair;
+
+                if (key == null)
+                    throw new ArgumentException("Key file is not a valid key.", nameof(rsaKeyFilePath));
+            }
+
+            ISigner signer = SignerUtilities.GetSigner("SHA1WITHRSA");
+            signer.Init(true, key.Private);
+
+            var input = Encoding.UTF8.GetBytes("Hello, World!");
+
+            signer.BlockUpdate(input, 0, input.Length);
+            byte[] privateKeySignature = signer.GenerateSignature();
+
+            byte[] agentSignature = _client.Sign(rsaAgentKey, input).Signature;
+
+            Assert.Equal(agentSignature, privateKeySignature);
+        }
+
+        [Fact]
+        public void CheckSignWithECDSAKey()
+        {
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+
+        private readonly SSHAgentClient _client;
     }
 }
